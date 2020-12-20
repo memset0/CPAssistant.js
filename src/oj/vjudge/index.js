0473f24d19500ff4686bb73f034ec2d1c8aebace
memset0 <34177126+memset0@users.noreply.github.com>
2020-12-20 10:15:19 +0800

    feat(vj): Count ths number of Accepted problems for each Online Judge on the user page

--- src/oj/vjudge/index.js
+++ src/oj/vjudge/index.js
@@ -73,3 +73,21 @@ $(document).on('click', '.modal-dialog td.RemoteRunId', function () {
 		utils.openInNewTab(remoteSubmissionUrl);
 	}
 });
+
+// feat: 个人信息界面统计通过题数
+if (location.pathname.startsWith('/user/')) {
+	$(document).on('click', '.toggle-detail', function () {
+		$("#probRecords tbody tr:not(#templ)").each(function () {
+			let oj_name = $(this).children('td').eq(0).text().trim();
+			let ac_num = $(this).children('td').eq(1).children('a').length;
+			let failed_num = $(this).children('td').eq(2).children('a').length;
+			$(this).children('td').eq(0).html(`
+				${oj_name}
+				<br>
+				<span style="color: #999">
+					${ac_num} / ${ac_num + failed_num}
+				</span>
+			`);
+		});
+	});
+}
