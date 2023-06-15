a6092b120a00d8db1084eb40659539a2caecf8f1
memset0 <34177126+memset0@users.noreply.github.com>
2023-06-15 23:26:23 +0800

    feat: remove custom style

--- src/modules/vjudge/features/CustomStyle.ts
+++
@@ -1,7 +0,0 @@
-import Feature from '../../../types/feature'
-import '../style/index.less'
-
-export default class CustomStyle extends Feature {
-	run() {
-	}
-}
--- src/modules/vjudge/main.ts
+++ src/modules/vjudge/main.ts
@@ -1,6 +1,5 @@
 import config from '../../config'
 import Module from "../../types/module"
-import CustomStyle from "./features/CustomStyle"
 import AcceptedCounter from "./features/AcceptedCounter"
 
 export default class ModuleVjudge extends Module {
@@ -10,7 +9,6 @@ export default class ModuleVjudge extends Module {
 	constructor() {
 		super('vjudge', config.match.vjudge)
 
-		this.register(new CustomStyle(this, 'custom-style'))
 		this.register(new AcceptedCounter(this, 'accepted-counter'))
 	}
 }
